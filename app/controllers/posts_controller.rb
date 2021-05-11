e3f7476f370451a46071c4f4b57eaf47bc323ba6
Sebastian Andres <sebastianandres@MacBook-Air-de-Sebastian.local>
2021-05-11 02:41:05 -0400

    clear_form_afterSend

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  def index
+    
+  end
+
+  # GET /posts/1
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    # @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+    
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+        unless @post.save
+            render json: @post.errors, status: :unprocessable_entity
+        end
+  end
+
+  # PATCH/PUT /posts/1
+  def update
+    # @post.update(post_params)
+      # redirect_to @post, notice: 'Post was successfully updated.'
+    # else
+    #   render :edit
+    # end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
